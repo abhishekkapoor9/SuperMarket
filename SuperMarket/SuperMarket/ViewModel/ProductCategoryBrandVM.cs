58fe5998f0150cd2f7ba244bf093399006555c7a
ManeeshR <ranjanmani020@gmail.com>
2017-07-27 15:58:58 +0530

    View Model

---
+++ SuperMarket/SuperMarket/ViewModel/ProductCategoryBrandVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarket.ViewModel
+{
+    public class ProductCategoryBrandVM
+    {
+        public string CategoryName { get; set; }
+        public byte[] CatogaryImage { get; set; }
+        public string SubCategoryName { get; set; }
+        public byte[] SubCategoryImage { get; set; }
+        public string ProductName { get; set; }
+        public string BrandName { get; set; }
+        public byte[] BrandImage { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public byte[] ProductImage { get; set; }
+    }
+}
