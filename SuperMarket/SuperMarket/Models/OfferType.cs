0a09962e9882a016f5de2c8238e8595beed40a78
Abhishek Kapoor <abhishekkpr9@gmail.com>
2017-07-27 12:19:07 +0530

    OfferType

---
+++ SuperMarket/SuperMarket/Models/OfferType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarket.Models
+{
+    public class OfferType
+    {
+        public int OfferTypeId { get; set; }
+        public string OfferName { get; set; }
+        public string OfferOn { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
