fe63766242c2af42bdb2e2531c856fd6bdb1d72a
Abhishek Kapoor <abhishekkpr9@gmail.com>
2017-07-27 12:26:35 +0530

    rate

---
+++ SuperMarket/SuperMarket/Models/Rate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarket.Models
+{
+    public class Rate
+    {
+        public int RateId { get; set; }
+        public string RateName { get; set; }
+    }
+}
