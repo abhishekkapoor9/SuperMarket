2b8d2b57373a9c61fda49cac8de2c19bd67c70bc
Abhishek Kapoor <abhishekkpr9@gmail.com>
2017-07-27 12:20:05 +0530

    Offer

---
+++ SuperMarket/SuperMarket/Models/Offer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarket.Models
+{
+    public class Offer
+    {
+        public int OfferId { get; set; }
+        public int OfferTypeId { get; set; }
+        public string CategoryOn { get; set; }
+        public string SubCategoryOn { get; set; }
+        public string ProductOn { get; set; }
+        public int discount% { get; set; }
+    }
+}
